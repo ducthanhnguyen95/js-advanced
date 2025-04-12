aaf99017822d18c0bcd5002e9aec1589f0455c52
thanhnd <ducthanhnguyen95@gmail.com>
2025-04-12 23:34:07 +0700

    Private Properties and Methods

---
+++ objects/private-properties-and-methods.js
@@ -0,0 +1,17 @@
+function Circle(radius) {
+  this.radius = radius;
+  let defaultLocation = { x: 0, y: 0 };
+  let computeOptimumLocation = function (factor) {
+    console.log('Computer optimum location');
+  };
+  this.draw = function () {
+    computeOptimumLocation(0.1);
+    
+    // defaultLocation;
+    // this.radius;
+    console.log('draw');
+  };
+}
+
+const circle = new Circle(10);
+circle.draw();
